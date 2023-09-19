6842b425bc5f7491ac76774e439b7280e912d80b
Vickyweb23 <128967705+Vickyweb23@users.noreply.github.com>
2023-09-19 13:29:10 +0530

    Add files via upload
    api project

---
+++ App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import Api from './api'
+
+function App() {
+  return (
+    <div className="App">
+      <Api/>
+    </div>
+  );
+}
+
+export default App;
---
+++ src1.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const src1 = () => {
+  return (
+    <div>src1</div>
+  )
+}
+
+export default src1
